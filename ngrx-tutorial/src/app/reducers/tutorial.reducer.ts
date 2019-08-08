0291c8a49b156680ab916e1639ab370d39c06b32
Tiago Avila <tiago.avila09@gmail.com>
2019-08-08 10:38:23 -0300

    full structure

---
+++ ngrx-tutorial/src/app/reducers/tutorial.reducer.ts
@@ -0,0 +1,24 @@
+import { Action } from '@ngrx/store'
+import { Tutorial } from './../models/tutorial.model'
+import * as TutorialActions from './../actions/tutorial.actions'
+
+// Section 1
+const initialState: Tutorial = {
+    name: 'Initial Tutorial',
+    url: 'http://google.com'
+}
+
+// Section 2
+export function reducer(state: Tutorial[] = [initialState], action: TutorialActions.Actions) {
+
+    // Section 3
+    switch(action.type) {
+        case TutorialActions.ADD_TUTORIAL:
+            return [...state, action.payload];
+        case TutorialActions.REMOVE_TUTORIAL:
+            state.splice(action.payload, 1)
+            return state;
+        default:
+            return state;
+    }
+}
